01f25cf17e441e44777e655272f858e2fecbe214
NovemberJr <jr.november@gmail.com>
2024-08-14 17:20:51 +0300

    removed unneccessary

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,8 @@
-import { useEffect } from 'react'
-//import useTelegram from './hooks/useTelegram'
-//import Header from './components/Header/Header'
-
 import './App.css'
 
 function App() {
-  /*const { toggleButton, tg } = useTelegram()
-
-  useEffect(() => {
-    tg.ready()
-  }, [])*/
-
   return (
     <>
-      {/*<Header />*/}
       <h1>Welcome to Web App</h1>
       <button>Toggle</button>
     </>
