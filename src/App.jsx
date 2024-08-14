d651eb0e456e55c8b0e0da93f6f3d07cdf8fbc76
NovemberJr <jr.november@gmail.com>
2024-08-14 17:17:34 +0300

    same

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 //import useTelegram from './hooks/useTelegram'
-import Header from './components/Header/Header'
+//import Header from './components/Header/Header'
 
 import './App.css'
 
